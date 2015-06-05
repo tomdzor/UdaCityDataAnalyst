f5944ed608c9a9390ba783530639ead88dd5a6b0
tomdzor <tomdzorevski@gmail.com>
2015-06-05 12:43:26 -0400

    Version 2

--- nyc_subway_project_1.py
+++ nyc_subway_project_1.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import scipy
+from scipy import stats
 import statsmodels.api as sm
+from datetime import datetime 
 
 path = r'D:\Toms\BigData\UdaCity\IntroToDataScience' + '\\'
 
@@ -19,7 +22,92 @@ def nyc_trunstile_V2():
 	
 	return pd.read_csv(path + 'turnstile_weather_v2.csv')
 
+def add_date_support_columns(data):
+	data['day_week'] = data['DATEn'].map(lambda x: datetime.strptime(x, '%m/%d/%Y').weekday())
+	data['holiday'] = data['DATEn'].map(lambda x: int(x == '5/30/2011'))
+	data['weekday'] = data['day_week'].map(lambda x: int(x > 4))
+	return
 	
+def workdays_from(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	workdays = data[(data['holiday'] == 0) &  (data['weekday'] == 0)]
+	
+	return workdays
+
+def weekends_from(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	weekends = data[(data['holiday'] == 0) &  (data['weekday'] == 1)]
+	
+	return weekends
+
+def holidays_from(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	holidays = data[(data['holiday'] == 1)]
+	
+	return holidays
+	
+def print_stats_for(data, title):
+	print('Stats: ' + title)
+	set = data['ENTRIESn_hourly']
+	mean = np.mean(set)
+	var = np.var(set)
+	count = len(set)
+	std = np.std(set)
+	print '\tBase' + '\n\t\tcount: ' + str(count) + '\n\t\tmean: ' + str(mean) + '\n\t\tvar: ' + str(var), '\n\t\tstd: ' + str(std)
+	set = data[data['rain'] == 0]['ENTRIESn_hourly']
+	mean = np.mean(set)
+	var = np.var(set)
+	count = len(set)
+	std = np.std(set)
+	print '\tno rain' + '\n\t\tcount: ' + str(count) + '\n\t\tmean: ' + str(mean) + '\n\t\tvar: ' + str(var), '\n\t\tstd: ' + str(std)
+	set = data[data['rain'] == 1]['ENTRIESn_hourly']
+	mean = np.mean(set)
+	var = np.var(set)
+	count = len(set)
+	std = np.std(set)
+	print '\train' + '\n\t\tcount: ' + str(count) + '\n\t\tmean: ' + str(mean) + '\n\t\tvar: ' + str(var), '\n\t\tstd: ' + str(std)
+	
+	return
+	
+def rain_work_day_no_holiday_from(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	rain = data[(data['holiday'] == 0) &  (data['weekday'] == 0) & (data['rain'] == 1) ]
+	
+	return rain
+
+def norain_work_day_no_holiday_from(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	rain = data[(data['holiday'] == 0) &  (data['weekday'] == 0) & (data['rain'] == 0) ]
+	
+	return rain
+		
+	
+
 		
 def scatter_plot_ridership_by_day(data):
 	'''
@@ -84,12 +172,34 @@ def scatter_plot_ridership_by_day(data):
 	plt.margins(0.2)
 	plt.subplots_adjust(bottom=0.25)
 	return plt
+
+def histograms_entries_hourly_rain_norain_for_all(data):
+	title = 'Histogram of Turnstile Entries Hourly'
+	return histograms_entries_hourly_rain_norain_2_subplots(data, title)
+
+def histograms_entries_hourly_rain_norain_for_workdays(data):
+	title = 'Histogram of Workday Turnstile Entries Hourly'
+	return histograms_entries_hourly_rain_norain_2_subplots(data, title)
+
+def histograms_entries_hourly_rain_norain_for_weekends(data):
+	title = 'Histogram of Weekend Turnstile Entries Hourly'
+	return histograms_entries_hourly_rain_norain_2_subplots(data, title)
+
+def histograms_entries_hourly_rain_norain_for_holidays(data):
+	title = 'Histogram of Holiday Turnstile Entries Hourly'
+	return histograms_entries_hourly_rain_for(data, title)
+
 	
-def histograms_entries_hourly_rain_norain_2_subplots(data):
+def histograms_entries_hourly_rain_norain_2_subplots(data, title):
 	'''
 	Return a plot containing 2 histogram subplots of entries 
 	hourly for sample rain and no-rain.
 	The x-axis is clipped at 6000.
+	Typical titles 
+	Histogram of Turnstile Entries Hourly
+	Histogram of Work Day Turnstile Entries Hourly
+	Histogram of Weekend Turnstile Entries Hourly
+	Histogram of Holiday Turnstile Entries Hourly
 	'''
 	
 	rain_df = data[data['rain'] == 1]
@@ -100,7 +210,7 @@ def histograms_entries_hourly_rain_norain_2_subplots(data):
 	plt.xlim(0,6000)
 	red_patch = mpatches.Patch(color='red', label='Rain')
 	plt.legend(handles=[red_patch])
-	plt.title('Histrogram of Turnstile Entries Hourly', fontsize=20)
+	plt.title(title, fontsize=20)
 
 	ax2= plt.subplot(2,1,2)
 	noRain_df['ENTRIESn_hourly'].hist(bins=250, color='blue')
@@ -114,6 +224,75 @@ def histograms_entries_hourly_rain_norain_2_subplots(data):
 	plt.legend(handles=[blue_patch])
 
 	return plt
+	
+def histograms_entries_hourly_rain_for(data, title):
+	'''
+	Return a plot containing 2 histogram subplots of entries 
+	hourly for sample rain and no-rain.
+	The x-axis is clipped at 6000.
+	Typical titles 
+	Histogram of Turnstile Entries Hourly
+	Histogram of Work Day Turnstile Entries Hourly
+	Histogram of Weekend Turnstile Entries Hourly
+	Histogram of Holiday Turnstile Entries Hourly
+	'''
+	
+	rain_df = data[data['rain'] == 1]
+	noRain_df = data[data['rain'] == 0]
+	fig = plt.figure() 
+	ax1 = plt.subplot(1,1,1)
+	rain_df['ENTRIESn_hourly'].hist(bins=250, color='red')
+	plt.xlim(0,6000)
+	red_patch = mpatches.Patch(color='red', label='Rain')
+	plt.legend(handles=[red_patch])
+	plt.title(title, fontsize=20)
+	plt.xlabel('Entries Hourly', fontsize= 16)
+	return plt
+	
+def rain_norain_Mann_Whitney_U_statistic_for(data):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	
+	rain = data[data['rain'] == 1]['ENTRIESn_hourly']
+	no_rain = data[data['rain'] == 0]['ENTRIESn_hourly']
+	
+	no_rain_mean = np.mean(no_rain)
+	rain_mean = np.mean(rain)
+	#print no_rain_mean, rain_mean
+	stats = scipy.stats.mannwhitneyu(rain, no_rain)
+	m_u = len(rain) *len(no_rain)/2.0
+	sigma_u = np.sqrt(len(rain)*len(no_rain)* (len(rain) + len(no_rain) + 1)/ 12)
+	u = stats[0]
+	z = (u - m_u)/sigma_u
+	pval = 2*scipy.stats.norm.cdf(z)
+	return len(rain), rain_mean, len(no_rain), no_rain_mean, stats[0], stats[1], z
+	
+def rainVsNoRainMann_Whitney_U_statistic(trunstile_weather):
+	'''
+	This function uses the turnstile_weather data frame and returns the following tuple 
+		1. The mean of the rain entries
+		2. The mean of the no rain entries
+		3. the Mann-Whitney U-Statistic and P-value for the rain and no-rain samples
+	'''
+	
+	rain = trunstile_weather[trunstile_weather['rain'] == 1]['ENTRIESn_hourly']
+	no_rain = trunstile_weather[trunstile_weather['rain'] == 0]['ENTRIESn_hourly']
+	
+	no_rain_mean = np.mean(no_rain)
+	rain_mean = np.mean(rain)
+	#print no_rain_mean, rain_mean
+	stats = scipy.stats.mannwhitneyu(rain, no_rain)
+	m_u = len(rain) *len(no_rain)/2.0
+	sigma_u = np.sqrt(len(rain)*len(no_rain)* (len(rain) + len(no_rain) + 1)/ 12)
+	u = stats[0]
+	z = (u - m_u)/sigma_u
+	pval = 2*scipy.stats.norm.cdf(z)
+	return rain_mean, no_rain_mean, stats[0], stats[1], z
+	
 	
 def ols_estimate_prediction(weather_turnstile):
 	'''
@@ -138,6 +317,64 @@ def ols_estimate_prediction(weather_turnstile):
 	est = est.fit()
 	#print(variables)
 	#print(est.summary())
-	prediction = est.predict(X)
+	predictions = est.predict(X)
 	
-	return est, prediction 
+	return est, predictions 
+	
+def histogram_residuals(turnstile_weather, predictions):
+	'''
+	Using the same methods that we used to plot a histogram of entries
+	per hour for our data, why don't you make a histogram of the residuals
+	(that is, the difference between the original hourly entry data and the predicted values).
+	Try different binwidths for your histogram.
+
+	Based on this residual histogram, do you have any insight into how our model
+	performed?  Reading a bit on this webpage might be useful:
+
+	http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
+	'''
+    
+	plt.figure()
+	ax1 = plt.subplot(1,1,1)
+	x = turnstile_weather['ENTRIESn_hourly'] - predictions
+	x.hist(bins=80, normed=True)
+	ax1.set_title('Histogram of OLS Residuals', fontsize= 20)
+	ax1.set_xlabel('Residual of Entries Hourly')
+	ax1.set_ylabel('Proportion')
+	return plt
+	
+def residuals_normal_probability_plot(turnstile_weather, predictions):
+	'''
+	Using the same methods that we used to plot a histogram of entries
+	per hour for our data, why don't you make a histogram of the residuals
+	(that is, the difference between the original hourly entry data and the predicted values).
+	Try different binwidths for your histogram.
+	Based on this residual histogram, do you have any insight into how our model
+	performed?  Reading a bit on this webpage might be useful:
+
+	http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
+	'''
+	plt.figure()
+	ax1 = plt.subplot(1,1,1)
+	x = turnstile_weather['ENTRIESn_hourly'] - predictions
+	stats.probplot(x, plot=ax1)
+	plt.title('OLS Residual Normal Probability Plot', fontsize=20)
+	return plt
+def residuals_vs_predictions_plot(turnstile_weather, predictions):
+	'''
+	Using the same methods that we used to plot a histogram of entries
+	per hour for our data, why don't you make a histogram of the residuals
+	(that is, the difference between the original hourly entry data and the predicted values).
+	Try different binwidths for your histogram.
+	Based on this residual histogram, do you have any insight into how our model
+	performed?  Reading a bit on this webpage might be useful:
+
+	http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
+	'''
+	
+	x = turnstile_weather['ENTRIESn_hourly'] - predictions
+	plt.figure() 
+	plt.scatter(x, predictions)
+	plt.title("OLS Residual vs Predictions", fontsize=20)
+	
+	return plt
